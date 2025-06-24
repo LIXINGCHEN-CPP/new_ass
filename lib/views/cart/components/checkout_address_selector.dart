53fdeae6d855dbd594f8b6e3a663fa8c9b9c6f3f
LIXINGCHEN-CPP <3414265673@qq.com>
2025-06-25 00:03:25 +0800

    add slection

--- lib/views/cart/components/checkout_address_selector.dart
+++ lib/views/cart/components/checkout_address_selector.dart
@@ -3,10 +3,28 @@ import 'package:flutter/material.dart';
 import '../../../core/components/title_and_action_button.dart';
 import 'checkout_address_card.dart';
 
-class AddressSelector extends StatelessWidget {
-  const AddressSelector({
-    super.key,
-  });
+class AddressSelector extends StatefulWidget {
+  const AddressSelector({super.key});
+
+  @override
+  State<AddressSelector> createState() => _AddressSelectorState();
+}
+
+class _AddressSelectorState extends State<AddressSelector> {
+  int selectedIndex = 1; // default second address active
+
+  final List<Map<String, String>> addresses = [
+    {
+      'label': 'Bell Suites',
+      'phone': '(60) 123-627-496',
+      'address': '10-02, Bell Suites, Kota Warisan',
+    },
+    {
+      'label': 'Xiamen University Malaysia',
+      'phone': '(60) 11-2423-4875',
+      'address': 'Jalan Sunsuria, Bandar Sunsuria',
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -15,23 +33,25 @@ class AddressSelector extends StatelessWidget {
         TitleAndActionButton(
           title: 'Select Delivery Address',
           actionLabel: 'Add New',
-          onTap: () {},
+          onTap: () {
+            // TODO: Navigate to add new address page
+          },
           isHeadline: false,
         ),
-        AddressCard(
-          label: 'Bell Suites',
-          phoneNumber: '(60) 123-627-496',
-          address: '10-02, Bell Suites, Kota Warisan',
-          isActive: false,
-          onTap: () {},
-        ),
-        AddressCard(
-          label: 'Xiamen University Malaysia',
-          phoneNumber: '(60) 11-2423-4875',
-          address: 'Jalan Sunsuria, Bandar Sunsuria',
-          isActive: true,
-          onTap: () {},
-        )
+        ...List.generate(addresses.length, (index) {
+          final addr = addresses[index];
+          return AddressCard(
+            label: addr['label']!,
+            phoneNumber: addr['phone']!,
+            address: addr['address']!,
+            isActive: selectedIndex == index,
+            onTap: () {
+              setState(() {
+                selectedIndex = index;
+              });
+            },
+          );
+        }),
       ],
     );
   }
